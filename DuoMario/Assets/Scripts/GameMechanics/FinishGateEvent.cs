512a3cc03608ca12c95022331a21f1c7ada65f3e
Alin Ursu <33602125+ursualin7890@users.noreply.github.com>
2022-03-31 21:44:46 +0300

    Revert "Added Intro and Win page"
    This reverts commit 0001d60fd704a8749d3d291b69044b00beaa7391.

--- DuoMario/Assets/Scripts/GameMechanics/FinishGateEvent.cs
+++ DuoMario/Assets/Scripts/GameMechanics/FinishGateEvent.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class FinishGateEvent : MonoBehaviour
 {
     private bool character1Arrived = false;
     private bool character2Arrived = false;
-
+    
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
@@ -19,30 +19,29 @@ public class FinishGateEvent : MonoBehaviour
         if (character1Arrived && character2Arrived)
         {
             Debug.Log("GAME OVER!");
-            SceneManager.LoadScene("Win");
         }
     }
-
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Character1")
         {
             character1Arrived = true;
         }
-
+        
         if (other.gameObject.tag == "Character2")
         {
             character2Arrived = true;
         }
     }
-
+    
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.tag == "Character1")
         {
             character1Arrived = false;
         }
-
+        
         if (other.gameObject.tag == "Character2")
         {
             character2Arrived = false;
